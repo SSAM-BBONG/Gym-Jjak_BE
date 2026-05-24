77813ef3d43cfd68ccb1d3493e205831364fd5e9
hyunj11 <rlaguswl1127@g.eulji.ac.kr>
2026-05-25 03:06:56 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/ssambbong/gymjjak/GymJjakApplication.java
@@ -0,0 +1,13 @@
+package com.ssambbong.gymjjak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymJjakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymJjakApplication.class, args);
+    }
+
+}
