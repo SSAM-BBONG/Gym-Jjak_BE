e63f612d9a39c47b4a681841ef73869d4aa53371
vinent-an <xxv211@naver.com>
2026-05-26 17:43:31 +0900

    ErrorCode enum 클래스 -> ErrorCode interface로 수정 후 각 도메인별로 구체화 하는걸로 수정함

--- src/test/java/com/ssambbong/gymjjak/global/domain/common/exception/ErrorCodeTest.java
+++ src/test/java/com/ssambbong/gymjjak/global/domain/common/exception/ErrorCodeTest.java
@@ -12,7 +12,7 @@ public class ErrorCodeTest {
     @Test
     void invalidInputErrorCodeHasExceptedValues() {
 
-        ErrorCode errorCode = ErrorCode.INVALID_INPUT;
+        ErrorCode errorCode = CommonErrorCode.INVALID_INPUT;
 
         assertThat(errorCode.getHttpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
         assertThat(errorCode.getCode()).isEqualTo("COMMON_400");
@@ -22,7 +22,7 @@ public class ErrorCodeTest {
     @DisplayName("INTERNAL_SERVER_ERROR 에러 코드는 500 상태를 가진다")
     @Test
     void internalServerErrorHasExpectedStatus() {
-        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
 
         assertThat(errorCode.getHttpStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
         assertThat(errorCode.getCode()).isEqualTo("COMMON_500");
