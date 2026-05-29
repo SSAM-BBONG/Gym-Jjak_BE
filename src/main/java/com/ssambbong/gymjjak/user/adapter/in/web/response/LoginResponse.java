d6dc53b2dcaede65032df5facb01e164954663f9
juwon <sjw4690@gmail.com>
2026-05-29 22:03:25 +0900

    accesstoken reissue modify

--- src/main/java/com/ssambbong/gymjjak/user/adapter/in/web/response/LoginResponse.java
+++ src/main/java/com/ssambbong/gymjjak/user/adapter/in/web/response/LoginResponse.java
@@ -6,8 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record LoginResponse(
         @Schema(description = "엑세스 토큰")
         String accessToken,
-        @Schema(description = "리프레시 토큰")
-        String refreshToken,
         @Schema(description = "온보딩 유무")
         boolean onboardingCompleted
 
