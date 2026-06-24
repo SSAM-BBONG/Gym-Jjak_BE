7f007af1841122f7078215d4fa43cb2ba70bab42
juwon <sjw4690@gmail.com>
2026-06-24 20:22:38 +0900

    refactoring

--- src/main/java/com/ssambbong/gymjjak/pt/ptReservation/presentation/api/CalendarPtController.java
+++ src/main/java/com/ssambbong/gymjjak/pt/ptReservation/presentation/api/CalendarPtController.java
@@ -7,9 +7,12 @@ import com.ssambbong.gymjjak.pt.ptReservation.application.usecase.FindPtReservat
 import com.ssambbong.gymjjak.pt.ptReservation.presentation.api.response.PtReservationCalendarResponse;
 import com.ssambbong.gymjjak.pt.ptReservation.presentation.api.response.PtReservationResponseCode;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +23,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/pt/reservations")
 @RequiredArgsConstructor
+@Validated
 public class CalendarPtController {
 
     private final FindPtReservationCalendarUsecase findPtReservationCalendar;
@@ -28,8 +32,8 @@ public class CalendarPtController {
     @Operation(summary = "캘린더 월별 조회", description = "일정을 월별로 조회한다.")
     public ResponseEntity<GlobalApiResponse<PtReservationCalendarResponse>> findPtReservationCalendar(
             @AuthenticationPrincipal AuthUser authUser,
-            @RequestParam int year,
-            @RequestParam int month
+            @RequestParam @Min(1) int year,
+            @RequestParam @Min(1) @Max(12) int month
     ) {
         List<PtReservationCalendarResult> results =
                 findPtReservationCalendar.findPtReservationCalendar(
