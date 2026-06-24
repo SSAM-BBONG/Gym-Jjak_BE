55e84f07c91f3ba0c26062f660886026f1b050bf
jaewon9804 <kumjaewon9804@g.eulji.ac.kr>
2026-06-24 11:14:22 +0900

    [refactor] 아이디 중복확인 별도 api 구현

--- src/main/java/com/ssambbong/gymjjak/organization/organizationApplication/presentation/api/response/OrganizationApplicationResponseCode.java
+++ src/main/java/com/ssambbong/gymjjak/organization/organizationApplication/presentation/api/response/OrganizationApplicationResponseCode.java
@@ -10,7 +10,8 @@ public enum OrganizationApplicationResponseCode implements ResponseCode {
     ORGANIZATION_APPLICATION_ALL_FOUND("ORG_200", "조직 신청 전체 목록 조회가 완료되었습니다."),
     ORGANIZATION_APPLICATION_APPROVED("ORG_200", "조직 신청이 승인되었습니다."),
     ORGANIZATION_APPLICATION_REJECTED("ORG_200", "조직 신청이 반려되었습니다."),
-    ORGANIZATION_APPLICATION_CANCEL("ORG_200", "조직 신청이 취소되었습니다.");
+    ORGANIZATION_APPLICATION_CANCEL("ORG_200", "조직 신청이 취소되었습니다."),
+    LOGINID_NOT_DUPLICATE("ORG_200","사용 가능한 ID입니다." );
 
     private final String code;
     private final String message;
