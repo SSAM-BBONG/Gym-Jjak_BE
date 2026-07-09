b60f8647fad277aecdf05a5e525e536f4f41c3bd
vinent-an <xxv211@naver.com>
2026-07-09 18:25:25 +0900

    코드래빗 수정

--- src/main/resources/db/migration/V2.8__add_organization_id_to_trainer_applications.sql
+++ src/main/resources/db/migration/V2.8__add_organization_id_to_trainer_applications.sql
@@ -1,10 +1,15 @@
 ALTER TABLE trainer_applications
     ADD COLUMN organization_id BIGINT NULL AFTER user_id;
 
+CREATE INDEX idx_trainer_applications_organization_status_created_id
+    ON trainer_applications (
+                             organization_id,
+                             status,
+                             created_at,
+                             trainer_application_id
+        );
+
 ALTER TABLE trainer_applications
     ADD CONSTRAINT fk_trainer_applications_organization
         FOREIGN KEY (organization_id)
             REFERENCES organizations (organization_id);
-
-CREATE INDEX idx_trainer_applications_organization_status_created_id
-    ON trainer_applications (organization_id, status, created_at, trainer_application_id);
