a8281d3eb32cc0d22fb7877220fe8f2419115e12
hyunj11 <rlaguswl1127@g.eulji.ac.kr>
2026-07-25 20:07:25 +0900

    피드백 수정

--- src/main/java/com/ssambbong/gymjjak/pt/feedback/presentation/api/request/UpdateFeedbackRequest.java
+++ src/main/java/com/ssambbong/gymjjak/pt/feedback/presentation/api/request/UpdateFeedbackRequest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public record UpdateFeedbackRequest(
         @NotBlank String content,
-        @Size(min = 1, max = 2) @Valid List<@NotNull MediaRequest> media
+        @Size(max = 2) @Valid List<@NotNull MediaRequest> media
 ) {
     public UpdateFeedbackCommand toCommand(Long userId, Long ptReservationId, Long feedbackId) {
         List<UpdateFeedbackCommand.MediaCommand> mediaCommands = (media == null) ? null :
